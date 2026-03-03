777c901b8c92bdf778ac364bd9fe89a807428f6b
Chetan Manker <chetanmanker67@gmail.com>
2026-03-03 17:08:02 +0530

    change in api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "https://crack4success-backend.onrender.com/api",
-  // baseURL: "http://localhost:5000/api",
-});
+// const api = axios.create({
+//   baseURL: "https://crack4success-backend.onrender.com/api",
+//   // baseURL: "http://localhost:5000/api",
+// });
     
+const api = axios.create({
+  baseURL: "/api",
+  withCredentials: true,
+});
 // Add a request interceptor
 api.interceptors.request.use(
   (config) => {
